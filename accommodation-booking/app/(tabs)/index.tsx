62415a31b248abc0355d9e7dcd93f84f45b8b0a5
sumanta ghosh <sumantaghosh5333@gmail.com>
2024-11-19 20:41:55 +0530

    create custom components and implement dark mode

--- accommodation-booking/app/(tabs)/index.tsx
+++ accommodation-booking/app/(tabs)/index.tsx
@@ -1,9 +1,9 @@
-import {StyleSheet, View, Text} from "react-native";
+import {View, Text} from "react-native";
 
 export default function TabOneScreen() {
   return (
     <View className="bg-red-700">
-      <Text className="text-green-700">Tab One</Text>
+      <Text className="text-white dark:text-black">Tab One</Text>
     </View>
   );
 }
